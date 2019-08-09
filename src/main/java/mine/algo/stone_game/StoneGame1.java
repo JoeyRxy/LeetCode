ea7147f7c1e5e5c79af0b5eb31e6d0558e33fc41
RXY <joeyrxy@gmail.com>
2019-08-09 22:07:03 +0800

    1

--- src/main/java/mine/algo/stone_game/StoneGame1.java
+++ src/main/java/mine/algo/stone_game/StoneGame1.java
@@ -5,15 +5,22 @@ package mine.algo.stone_game;
  */
 public class StoneGame1 {
 
+    public StoneGame1() {
+    }
+
     private int[] piles;
 
     public boolean stoneGame(int[] piles) {
         this.piles = piles;
     }
 
-    public int recursive(int i,int j) {
+    public int recursive(int i, int j) {
         if (i == j)
             return piles[i];
-        return Math.max(piles[i]-recursive(i+1, j),piles[j])
+        return Math.max(piles[i] - recursive(i + 1, j), piles[j]);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
